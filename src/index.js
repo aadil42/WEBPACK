9240a17dd0291d1ef9fd69cf4b6e433f2c71172c
aadil42 <adilkelawala@gmail.com>
2022-03-30 18:48:42 +0530

    initial commit. commiting from the middle of the course so the majority of file might have the same commit massage

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import HelloBtn from './components/hello-btn/hello-btn.js';
+import Heading from './components/heading/heading.js';
+
+
+
+
+const heading = new Heading();
+heading.render();
+
+const hello_btn = new HelloBtn();
+hello_btn.render();
